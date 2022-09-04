e4152f7aa8e43009cd421d8586694906ae452e06
Matthaus Nawan <matthaus@Matthauss-iMac.local>
2022-09-04 14:37:27 -0300

    category-modal

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { Modal } from "react-native";
 import {
-  Container,
-  Header,
-  Title,
-  Form,
-  Fields,
-  TransactionsTypes,
+	Container,
+	Header,
+	Title,
+	Form,
+	Fields,
+	TransactionsTypes,
 } from "./styles";
 import { Input } from "../../components/Forms/Input";
 import { Button } from "../../components/Forms/Button";
@@ -16,63 +16,63 @@ import { CategorySelectButton } from "../../components/Forms/CategorySelectButto
 import { CategorySelect } from "../../screens/CategorySelect";
 
 export function Register() {
-  const [transactionType, setTransactionType] = useState("");
-  const [categoryMotalOpen, setCategoryModalOpen] = useState(false);
-  const [category, setCategory] = useState({
-    key: "category",
-    name: "Categoria",
-  });
+	const [transactionType, setTransactionType] = useState("");
+	const [categoryMotalOpen, setCategoryModalOpen] = useState(false);
+	const [category, setCategory] = useState({
+		key: "category",
+		name: "Categoria",
+	});
 
-  function handleTransactionTypeSelect(type: "up" | "down") {
-    setTransactionType(type);
-  }
+	function handleTransactionTypeSelect(type: "up" | "down") {
+		setTransactionType(type);
+	}
 
-  function handleCloseSelectedCategory() {
-    setCategoryModalOpen(false);
-  }
+	function handleCloseSelectedCategory() {
+		setCategoryModalOpen(false);
+	}
 
-  function handleOpenSelectedCategory() {
-    setCategoryModalOpen(true);
-  }
+	function handleOpenSelectedCategory() {
+		setCategoryModalOpen(true);
+	}
 
-  return (
-    <Container>
-      <Header>
-        <Title>Cadastro</Title>
-      </Header>
-      <Form>
-        <Fields>
-          <Input placeholder="Nome" />
-          <Input placeholder="Preço" />
-          <TransactionsTypes>
-            <TransactionTypeButton
-              isActive={transactionType === "up"}
-              type="up"
-              title="Income"
-              onPress={() => handleTransactionTypeSelect("up")}
-            />
-            <TransactionTypeButton
-              isActive={transactionType === "down"}
-              type="down"
-              title="OutCome"
-              onPress={() => handleTransactionTypeSelect("down")}
-            />
-          </TransactionsTypes>
-          <CategorySelectButton
-            title="Categoria"
-            onPress={handleOpenSelectedCategory}
-          />
-        </Fields>
-        <Button title="Enviar" />
-      </Form>
+	return (
+		<Container>
+			<Header>
+				<Title>Cadastro</Title>
+			</Header>
+			<Form>
+				<Fields>
+					<Input placeholder="Nome" />
+					<Input placeholder="Preço" />
+					<TransactionsTypes>
+						<TransactionTypeButton
+							isActive={transactionType === "up"}
+							type="up"
+							title="Income"
+							onPress={() => handleTransactionTypeSelect("up")}
+						/>
+						<TransactionTypeButton
+							isActive={transactionType === "down"}
+							type="down"
+							title="OutCome"
+							onPress={() => handleTransactionTypeSelect("down")}
+						/>
+					</TransactionsTypes>
+					<CategorySelectButton
+						title={category.name}
+						onPress={handleOpenSelectedCategory}
+					/>
+				</Fields>
+				<Button title="Enviar" />
+			</Form>
 
-      <Modal visible={categoryMotalOpen}>
-        <CategorySelect
-          category={category}
-          setCategory={setCategory}
-          closeSelectCategory={handleCloseSelectedCategory}
-        />
-      </Modal>
-    </Container>
-  );
+			<Modal visible={categoryMotalOpen}>
+				<CategorySelect
+					category={category}
+					setCategory={setCategory}
+					closeSelectCategory={handleCloseSelectedCategory}
+				/>
+			</Modal>
+		</Container>
+	);
 }
